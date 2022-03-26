9bbd78838b816a9b8f0543de1c2fb9ad1da17bdb
Akash NP <akashnp1998@gmail.com>
2022-03-26 14:03:42 +0530

    Install docx-mailmerge library, add initial code
    This libray brings in the exact functionality we want, testing code

--- merger.py
+++ merger.py
@@ -1,12 +1,11 @@
 import csv
-
-message = """
-Hi {Name} , your email is {Email}
-"""
+from mailmerge import MailMerge
 
 with open('contacts.csv') as file:
     reader = csv.reader(file)
     next(reader)
     for Name, Email in reader:
-        print(message.format(Name=Name, Email=Email))
+        with MailMerge('Test.docx') as content_template:
+            print(content_template.get_merge_fields())
+        
         
