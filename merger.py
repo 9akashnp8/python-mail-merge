b4952ad5801d5bdeb675c27f0b826680f818966a
Akash NP <akashnp1998@gmail.com>
2022-03-26 14:11:31 +0530

    Add docx2pdf module to batch convert merged docx files to pdf

--- merger.py
+++ merger.py
@@ -1,5 +1,7 @@
 import csv
+import time
 from mailmerge import MailMerge
+from docx2pdf import convert
 
 with open('contacts.csv') as file:
     reader = csv.reader(file)
@@ -7,6 +9,9 @@ with open('contacts.csv') as file:
     for Name, Email in reader:
         with MailMerge('Test.docx') as content_template:
             content_template.merge(Name=Name, Email=Email)
-            content_template.write(f'E:\Private\Programming\Python\mail-merger\docx\{Name}.docx')
+            content_template.write(f'E:\Private\Programming\Python\mail-merger\docx_files\{Name}.docx')
+
+time.sleep(5)
+convert("docx_files/")
         
         
